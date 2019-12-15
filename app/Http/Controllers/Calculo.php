77b5f79f8cef66bab9832d49bd6f4c11979caaa9
ViniciosTI <0vinicios1@gmail.com>
2019-12-15 02:28:13 -0300

    Resolvido o calculo e a porcentagem, separado os metodos e os comentários

--- app/Http/Controllers/Calculo.php
+++ app/Http/Controllers/Calculo.php
@@ -4,64 +4,63 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 class Calculo extends Controller{
+    /*
+        $request -> request que veio da calculadora
+        return -> devolve o resultado para a calculadora
+    */
     public function calcular(Request $request){
-        $req = $request->json()->all();
+        return $this->resolveCalculo($request->json()->all());
+    }
+    /*
+        $request -> request que veio da calculadora
+        return -> devolve o resultado para a calculadora
+    */
+    public function porcentagem(Request $request){
+        $resultado = $this->resolveCalculo($request->json()->all());
+        $resultado -= $request->json()->get(count($request->json())-1);
+        return abs(($resultado * $request->json()->get(count($request->json())-1)) / 100);
+    }
+    /*
+        $req-> lista de valores para serem calculados
+        return -> resultados de todas as operações dos valores 
+    */
+    public function resolveCalculo($req){
         $calculo = 0;
         $num = $req[0];
         $memoria = "";
         for($i = 0; $i < count($req); $i++){
             if (is_numeric($req[$i])){
                 $num = $req[$i];
-                switch($memoria){
-                    case "+": 
-                        $calculo = $calculo + $num; 
-                    break;
-                    case "-": 
-                        $calculo = $calculo - $num; 
-                    break;
-                    case "x": 
-                        $calculo = $calculo * $num; 
-                    break;
-                    case "/": 
-                        $calculo = $calculo / $num; 
-                    break;
-                    default:
-                        $calculo = $num;
-                    break;
-                }
+                $calculo = $this->calculaOperado($memoria, $calculo, $num);
                 $memoria = "";
             } else { $memoria = $req[$i]; }
         }     
         return $calculo;
     }
-    public function porcentagem(Request $request){
-        $req = $request->json()->all();
-        $calculo = 0;
-        $num = $req[0];
-        $memoria = "";
-        for($i = 0; $i < count($req); $i++){
-            if (is_numeric($req[$i])){
-                $num = $req[$i];
-                switch($memoria){
-                    case "+": 
-                        $calculo = $calculo + $num; 
-                    break;
-                    case "-": 
-                        $calculo = $calculo - $num; 
-                    break;
-                    case "x": 
-                        $calculo = $calculo * $num; 
-                    break;
-                    case "/": 
-                        $calculo = $calculo / $num; 
-                    break;
-                    default:
-                        $calculo = $num;
-                    break;
-                }
-                $memoria = "";
-            } else { $memoria = $req[$i]; }
-        }     
-        return $calculo;
+    /*
+        $o-> operador utilizado no calculo
+        $n1 -> primeiro valor para o calculo
+        $n2 -> segundo valor para o calculo
+        return -> valor já calculado
+    */
+    public function calculaOperado($o, $n1, $n2){
+        switch($o){
+            case "+": 
+                $n1 = $n1 + $n2; 
+            break;
+            case "-": 
+                $n1 = $n1 - $n2; 
+            break;
+            case "x": 
+                $n1 = $n1 * $n2; 
+            break;
+            case "/": 
+                $n1 = $n1 / $n2; 
+            break;
+            default:
+                $n1 = $n2;
+            break;
+        }
+        return $n1;
     }
 }
